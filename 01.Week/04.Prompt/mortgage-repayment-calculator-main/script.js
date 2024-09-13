e6fb69350829cded492fa20ca84e94439abb3aed
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-13 14:01:05 +0530

    Design ✅

---
+++ 01.Week/04.Prompt/mortgage-repayment-calculator-main/script.js
@@ -0,0 +1,6 @@
+// let Amount = document.getElementById("amount").value;
+let Time = document.getElementById("time");
+let Rate = document.getElementById("rate");
+let Result = document.getElementById("result");
+let Total = document.getElementById("total_repayment");
+

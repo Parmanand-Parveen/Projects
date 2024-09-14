cce8fc65081181becf135998b3b84d789e609bb3
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-14 11:52:06 +0530

    Prompt 4 ✅

--- 01.Week/04.Prompt/mortgage-repayment-calculator-main/script.js
+++ 01.Week/04.Prompt/mortgage-repayment-calculator-main/script.js
@@ -1,6 +1,33 @@
-// let Amount = document.getElementById("amount").value;
+let Amount = document.getElementById("amount");
 let Time = document.getElementById("time");
 let Rate = document.getElementById("rate");
 let Result = document.getElementById("result");
-let Total = document.getElementById("total_repayment");
+let total_repayment = document.getElementById("total_repayment");
+let clears = document.getElementById("clear");
 
+console.log(Amount.value);
+
+function getpayment() {
+  let amount = Amount.value;
+  let time = Time.value * 12;
+  let rate = Rate.value / 100 / 12;
+
+  let m =
+    [amount * rate * Math.pow(1 + rate, time)] / (Math.pow(1 + rate, time) - 1);
+  Result.value = m.toFixed(2);
+
+  let payment = m * Time.value * 12;
+
+  total_repayment.value = payment.toFixed(2);
+}
+
+clears.addEventListener("click", clear);
+
+function clear() {
+  Amount.value = "";
+  Time.value = "";
+  Rate.value = "";
+  Result.value = "";
+  total_repayment.value = "";
+  console.log("clear");
+}

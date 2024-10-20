e7d9c5da4dd3f72efa12af3644caef50aecb3846
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-20 22:29:49 +0530

    Header✅

--- 05.Week/Major Website/week5_major_project/src/components/Header.jsx
+++ 05.Week/Major Website/week5_major_project/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <nav className='flex justify-between items-center  bg-zinc-900 text-white px-12 py-4'>
+    <div className='sticky top-0 z-10'>
+        <nav className='flex justify-between items-center  bg-zinc-900 text-white px-2 lg:px-12 py-4'>
           <div className='text-3xl'>Logo</div>
-            <ul className='flex gap-3 list-none'>
+            <ul className='hidden lg:flex gap-3 list-none'>
               <li className='hover:bg-gradient-to-r from-red-600 to-indigo-400 hover:inline-block hover:text-transparent hover:bg-clip-text transition-all'>Home</li>
               <li className='hover:bg-gradient-to-r from-red-600 to-indigo-400 hover:inline-block hover:text-transparent hover:bg-clip-text transition-all'>About</li>
               <li  className='hover:bg-gradient-to-r from-red-600 to-indigo-400 hover:inline-block hover:text-transparent hover:bg-clip-text transition-all'>Courses</li>

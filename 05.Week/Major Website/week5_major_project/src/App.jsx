1f7406cb7dcfb1bd60feb3d16a4569521c331a15
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-21 01:18:31 +0530

    Done✅

--- 05.Week/Major Website/week5_major_project/src/App.jsx
+++ 05.Week/Major Website/week5_major_project/src/App.jsx
@@ -2,13 +2,27 @@ import { Fragment, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Herosection from './components/Hero-section'
-function App() {
- 
+import Info from './components/Info'
+import WhyUs from './components/WhyUs'
+import FeaturedCourses from './components/FeaturedCourses'
+import OurInstructor from './components/OurInstructor'
+import Testimonials from './components/Testimonials'
+import ContactUs from './components/ContactUs'
+import Footer from './components/Footer'
 
+function App() {
+   
   return (
-    <Fragment>
+    <Fragment clasName="overflow-x-hidden">
       <Header />
       <Herosection/>
+      <Info/> 
+      <WhyUs/>
+      <FeaturedCourses/>
+      <OurInstructor/>
+      <Testimonials/>
+      <ContactUs/>
+      <Footer/>
     </Fragment>
   )
 }
